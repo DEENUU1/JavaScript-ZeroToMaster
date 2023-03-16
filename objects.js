d65a6e02a49b63b13fb840b78e47058407f90a78
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-16 16:22:22 +0100

    Section 2 | Objects first task | Create 2 objects: - car - email Print them in the console

---
+++ objects.js
@@ -0,0 +1,18 @@
+let car = {
+    brand: "fiat",
+    engine: 1.4,
+    max_speed: 60,
+    year: "2003",
+    status: "used"
+  };
+
+  let email = {
+    name: "Kacper",
+    lastname: "Kowalski",
+    host: "@protonmail",
+    account_count: 2,
+    emailAddress: "kacperkowalski@protonmail.com"
+  };
+
+  console.log(car);
+  console.log(email);
