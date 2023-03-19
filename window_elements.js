2cf808e366eb47b56920a3e32f7a38b8aa4163e1
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-19 21:27:45 +0100

    Section 2 | Window elements | working with prompt andsetInterval functions

---
+++ window_elements.js
@@ -0,0 +1,7 @@
+let userHours = prompt("Ile godzin dziennie korzystasz z PC?");
+let betterHours = userHours / 2;
+alert(`Powinieneś korzystać ${betterHours} godzin`);
+
+  setInterval(() => {
+     console.log(innerHeight, innerWidth) 
+  }, 1000);
