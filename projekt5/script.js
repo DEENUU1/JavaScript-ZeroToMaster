9dfee4489578c7f2a2f5b9464188ed0a291c4f65
Kacper Włodarczyk <kacperwlodarczyk@protonmail.com>
2023-03-28 21:17:21 +0200

    Section 13 | Project 5 | Main code of the application which allows to save book title and auther. Display them in the table, delete from table and store in local storage. Class book allows to store data like: title, author and id which is a date of adding book BookList allow to save and delete  book and store them in local storage class Ui allows to display saved books in table and display delete button class storage allows to store books in local storage

--- projekt5/script.js
+++ projekt5/script.js
@@ -1,3 +1,124 @@
 window.onload = function () {
     console.log("work");
+    booksList.init();
+};
+
+
+class Book {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+        this.id = Date.now();
+    }
 }
+
+class BooksList {
+    constructor () {
+        this.books = [];
+    }
+
+    init() {
+        document.getElementById("saveButton").addEventListener("click", 
+        (event) => this.saveButton(event));
+        
+        this.loadDataFromStorage();
+    }
+
+    loadDataFromStorage() {
+        const data = storage.getItems();
+        if(data == null || data == undefined) return;
+        this.books = data;
+        data.forEach((value, index) => {
+            ui.addBookToTable(value);
+        })
+    }
+
+    saveButton(event) {
+        const author = document.getElementById("bookAuthor").value;
+        const title = document.getElementById("bookTitle").value;
+        event.preventDefault();
+        if(author === "" || title === "") {
+            alert("null value");
+            return;
+        }
+
+        const book = new Book(title, author);
+        this.addBook(book);
+    }
+
+    addBook(book) {
+        this.books.push(book);
+        ui.addBookToTable(book);
+        this.saveData();
+    }
+
+    removeBookById(bookId) {
+        this.books.forEach((element, index) => {
+            if(element.id == bookId) this.books.splice(index, 1);
+        });
+
+        this.saveData();
+    }
+
+    saveData() {
+        storage.saveItems(this.books);
+    }
+}
+
+
+
+const booksList = new BooksList();
+
+class Ui {
+
+    deleteBook(event) {
+        const bookId = event.target.getAttribute("data-book-id");
+        event.target.parentElement.parentElement.remove();
+        booksList.removeBookById(bookId);
+    }
+
+    addBookToTable(book) {
+        const tbody = document.querySelector("#booksTable tbody");
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <td> ${book.title} </td>
+            <td> ${book.author} </td>
+            <td> 
+                <button type="button" data-book-id="${book.id}" class="btn btn-danger brn-small delete">Delete</button>
+            </td>
+        `;
+        tbody.appendChild(tr);
+
+        let deleteButton = document.querySelector(`button.delete[data-book-id='${book.id}']`)
+        deleteButton.addEventListener("click", (event) => this.deleteBook(event));
+        this.clearForm();
+    }
+
+    clearForm() {
+        document.getElementById("bookTitle").value = "";
+        document.getElementById("bookAuthor").value = "";
+    }
+}
+
+const ui = new Ui();
+
+
+class Storage {
+
+    getItems() {
+        let books = null;
+        if(localStorage.getItem("books") !== null){
+            books = JSON.parse(localStorage.getItem("books"));
+        } else {
+            books = [];
+        }
+
+        return books;
+    }
+
+    saveItems (books) {
+        localStorage.setItem("books", JSON.stringify(books));
+    }
+}
+
+const storage = new Storage();
